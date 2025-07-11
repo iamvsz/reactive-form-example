677a129d81c2dc21b0e62d0cfe56ab75c9b3fa85
Vishal Zade <vishalzade75@gmail.com>
2025-07-11 10:00:44 +0530

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'reactive-form-example';
+  userForm : FormGroup;
+  constructor() {
+    this.userForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      age: new FormControl('', [Validators.required, Validators.min(18), Validators.max(100)]),
+    })
+
+    this.userForm.valueChanges.subscribe(value => {
+      console.log('Form changes:', value);
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Form submitted:', this.userForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
